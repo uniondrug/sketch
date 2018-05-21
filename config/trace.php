94e4d6fa017338a972e1f2c782ffadd229885308
fuyibing <websearch@163.com>
2018-05-21 13:54:42 +0800

    Update trace.php
    update config as domain

--- config/trace.php
+++ config/trace.php
@@ -3,14 +3,21 @@
  * 调用链跟踪服务器设置
  *
  * HTTP方式的采集：
- * enable:  是否将trace信息上报
- * service: 采集服务地址，可以是 tcp://ip:port or http://ip:port
+ * service: 采集API的完整路径，可以是 tcp://ip:port or http://ip:port，也可是一个在服务中心注册的服务，比如：trace
  * timeout: 超时时间
  */
 return [
     'default' => [
-        'enable'  => true,
+        'timeout' => 30,
         'service' => 'http://127.0.0.1:8081',
-        'timeout' => 10,
     ],
+    'development' => [
+        'service' => 'http://trace.module.dev.turboradio.cn:8081',
+    ],
+    'testing' => [
+        'service' => 'http://trace.module.test.turboradio.cn:8081',
+    ],
+    'production' => [
+        'service' => 'http://trace.module.uniondrug.cn:8081',
+    ]
 ];
